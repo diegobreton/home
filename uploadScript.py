1b2658d02a188a12ea8d310ca05219ff6c36aad9
Diego Breton <diegobretonp@gmail.com>
2019-04-14 18:59:58 -0700

    correct python file

--- uploadScript.py
+++ uploadScript.py
@@ -3,15 +3,20 @@ from io import BytesIO
 import zipfile
 import mimetypes
 
-s3 = boto3.resource('s3')
-filesBucket = s3.Bucket('files.diegobreton.com')
-buildBucket = s3.Bucket('filesbuild.diegobreton.com')
+def lambda_handler(event, context):
 
-zipFile = BytesIO()
-buildBucket.download_fileobj('buildFiles.zip', zipFile)
 
-with zipfile.ZipFile(zipFile) as myZip:
-    for nm in myZip.namelist():
-        obj = myZip.open(nm)
-        filesBucket.upload_fileobj(obj, nm, ExtraArgs={'ContentType': mimetypes.guess_type(nm)[0]})
-        filesBucket.Object(nm).Acl().put(ACL='public-read')
+    s3 = boto3.resource('s3')
+    filesBucket = s3.Bucket('files.diegobreton.com')
+    buildBucket = s3.Bucket('filesbuild.diegobreton.com')
+
+    zipFile = BytesIO()
+    buildBucket.download_fileobj('buildFiles.zip', zipFile)
+
+    with zipfile.ZipFile(zipFile) as myZip:
+        for nm in myZip.namelist():
+            obj = myZip.open(nm)
+            filesBucket.upload_fileobj(obj, nm, ExtraArgs={'ContentType': mimetypes.guess_type(nm)[0]})
+            filesBucket.Object(nm).Acl().put(ACL='public-read')
+
+    return 'Executed upload script'
